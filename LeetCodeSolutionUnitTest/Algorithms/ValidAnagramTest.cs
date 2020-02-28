6f76b217224f4142d6aa6c099cce3f25c71264f9
weihanli <weihanli@outlook.com>
2020-02-29 01:08:09 +0800

    ValidAnagram

---
+++ LeetCodeSolutionUnitTest/Algorithms/ValidAnagramTest.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeSolutions.Algorithms;
+using Xunit;
+
+namespace LeetCodeSolutions.UnitTest.Algorithms
+{
+    public class ValidAnagramTest
+    {
+        [Theory]
+        [InlineData("anagram", "nagaram", true)]
+        [InlineData("rat", "car", false)]
+        public void Sln1Test(string s, string t, bool valid)
+        {
+            Assert.Equal(valid, ValidAnagram.Solution1(s, t));
+        }
+    }
+}
---
+++ LeetCodeSolutions/Algorithms/ValidAnagram.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace LeetCodeSolutions.Algorithms
+{
+    // https://leetcode-cn.com/problems/valid-anagram/
+    public class ValidAnagram
+    {
+        /// <summary>
+        /// 解决方案1
+        /// Sort 之后比较
+        /// </summary>
+        /// <param name="s">s1</param>
+        /// <param name="t">t1</param>
+        /// <returns>isValid</returns>
+        public static bool Solution1(string s, string t)
+        {
+            if (s == t)
+            {
+                return true;
+            }
+            if (s == null || t == null)
+            {
+                return false;
+            }
+
+            return s.OrderBy(_ => _).SequenceEqual(t.OrderBy(_ => _));
+        }
+    }
+}
