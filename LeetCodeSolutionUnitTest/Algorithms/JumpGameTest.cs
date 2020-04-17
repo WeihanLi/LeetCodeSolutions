9301efac84ccecaee1357cd4423fc0baf07b6dd8
weihanli <weihanli@outlook.com>
2020-04-17 14:12:42 +0800

    add JumpGame solution

---
+++ LeetCodeSolutionUnitTest/Algorithms/JumpGameTest.cs
@@ -0,0 +1,24 @@
+﻿using LeetCodeSolutions.Algorithms;
+using Xunit;
+
+namespace LeetCodeSolutions.UnitTest.Algorithms
+{
+    public class JumpGameTest
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 3, 1, 1, 4 }, true)]
+        [InlineData(new int[] { 3, 2, 1, 0, 4 }, false)]
+        public void Solution1Test(int[] numbers, bool result)
+        {
+            Assert.Equal(result, JumpGame.Solution1(numbers));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 2, 3, 1, 1, 4 }, true)]
+        [InlineData(new int[] { 3, 2, 1, 0, 4 }, false)]
+        public void Solution2Test(int[] numbers, bool result)
+        {
+            Assert.Equal(result, JumpGame.Solution2(numbers));
+        }
+    }
+}
---
+++ LeetCodeSolutions/Algorithms/JumpGame.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace LeetCodeSolutions.Algorithms
+{
+    public class JumpGame
+    {
+        public static bool Solution1(int[] nums)
+        {
+            if (nums == null || nums.Length <= 1)
+            {
+                return true;
+            }
+
+            var n = 1;
+            for (var i = nums.Length - 2; i >= 0; i--)
+            {
+                if (nums[i] >= n)
+                {
+                    n = 1;
+                }
+                else
+                {
+                    n++;
+                }
+                if (i == 0 && n > 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool Solution2(int[] nums)
+        {
+            if (nums == null || nums.Length <= 1)
+            {
+                return true;
+            }
+
+            int start = 0, end = 0;
+            while (start <= end)
+            {
+                // 能达到的最远的位置
+                end = Math.Max(end, start + nums[start]);
+                if (end >= nums.Length - 1)
+                {
+                    return true;
+                }
+
+                // 向后移动
+                start++;
+            }
+            return end >= nums.Length - 1;
+        }
+    }
+}
